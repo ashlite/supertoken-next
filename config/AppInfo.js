42d677befe3d3739659c6d7603db178bd726b22f
David.S <david91santoso@gmail.com>
2022-06-20 17:46:46 +0700

    update minor production

--- config/AppInfo.js
+++ config/AppInfo.js
@@ -1,17 +1,17 @@
 //DEV
-export const appInfo = {
-  appName: 'aha-test-next',
-  apiDomain: 'http://localhost:3000',
-  websiteDomain: 'http://localhost:3000',
-  apiBasePath: '/api/auth',
-  websiteBasePath: '/auth'
-}
-
-//PROD
 // export const appInfo = {
 //   appName: 'aha-test-next',
-//   apiDomain: 'https://aha-test-for-david.herokuapp.com/',
-//   websiteDomain: 'https://aha-test-for-david.herokuapp.com/',
+//   apiDomain: 'http://localhost:3000',
+//   websiteDomain: 'http://localhost:3000',
 //   apiBasePath: '/api/auth',
 //   websiteBasePath: '/auth'
 // }
+
+//PROD
+export const appInfo = {
+  appName: 'aha-test-next',
+  apiDomain: 'https://aha-test-for-david.herokuapp.com/',
+  websiteDomain: 'https://aha-test-for-david.herokuapp.com/',
+  apiBasePath: '/api/auth',
+  websiteBasePath: '/auth'
+}
